63165b546b0127fe318b1cb074bb32154c4b1860
Maurice-Hayward <mauricehayward1@gmail.com>
2017-08-14 17:55:42 -0400

    vuex

--- Stock_Trader/src/main.js
+++ Stock_Trader/src/main.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 
 import App from "./App.vue";
 import { routes } from "./routes.js";
+import store from './store/store'
 
 Vue.use(VueRouter);
 
@@ -11,5 +12,6 @@ const router = new VueRouter({ mode: "history", routes });
 new Vue({
   el: "#app",
   router,
+  store,
   render: h => h(App)
 });
---
+++ Stock_Trader/src/store/modules/stocks.js
@@ -0,0 +1,37 @@
+import stocks from "../../data/stocks";
+
+const state = {
+  stocks: []
+};
+
+const mutations = {
+  'SET_STOCKS'(state, stock) {
+    state.stocks = stocks;
+  },
+  'RND_STOCK'(state) {}
+};
+
+const actions = {
+  buyStock: ({ commit }, order) => {
+    commit();
+  },
+  initStocks: ({ commit }) => {
+    commit("SET_STOCKS", stocks);
+  },
+  randomizeStocks: ({ commit }) => {
+    commit("RND_STOCK");
+  }
+};
+
+const getters = {
+  stocks: state => {
+    return state.stocks;
+  }
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+};
